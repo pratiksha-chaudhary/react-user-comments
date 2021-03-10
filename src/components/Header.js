2cb15a4cfd8e2dac7fc229488bbe6767560c425c
pratiksha-chaudhary <pratiksha.chaudhary89@gmail.com>
2021-03-11 00:28:13 +0100

    created skeleton code

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <div className="flex-container">
+        <div className="header-logo">LiveEO</div>
+        <div className="header-items">
+          <div className="header-item">Map</div>
+          <div className="header-item">Tasks</div>
+          <div className="header-item">Dashboard</div>
+          <div className="header-item">Comments</div>
+          <div className="header-item">Admin</div>
+          <div className="header-item">L</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
