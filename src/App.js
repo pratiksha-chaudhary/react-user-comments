db6878650f4f24db142924429a8527c093b9d80e
pratiksha-chaudhary <pratiksha.chaudhary89@gmail.com>
2021-03-11 19:51:02 +0100

    created comments component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import Header from "./components/Header";
+import Comments from "./components/Comments";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
-      <div>sdfjhsdh</div>
+      <Comments />
     </div>
   );
 }
---
+++ src/components/Comments.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Comments() {
+  return (
+    <div className="comments-app">
+      <div className="comments-app-header">
+        <div className="comments-header">Comments</div>
+        <div className="create-comment">Create comment</div>
+      </div>
+      <div className="user-comments"></div>
+    </div>
+  );
+}
+
+export default Comments;
